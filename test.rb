817a432fb4e9f3803fd3a92e33b9653326b65e47
Rocio <mroguzman@gmail.com>
2015-03-11 19:09:30 +0100

    parseo wiki github to json

--- test.rb
+++ test.rb
@@ -1,50 +1,75 @@
 URL_BASE = "https://github.com"
-MONTHS =  [nil] + %w(Enero Febrero Marzo Abril Mayo Junio Julio Agosto Septiembre Octubre Noviembre Diciembre)
-
-#Check || install nokogiri
-begin
-  gem 'nokogiri'
-rescue LoadError
-  system("gem install nokogiri")
-  Gem.clear_paths
-end
+months =  %w(Enero Febrero Marzo Abril Mayo Junio Julio Agosto Septiembre Octubre Noviembre Diciembre)
 
 require 'nokogiri'
 require 'open-uri'
 require 'json'
 
-def build_json(data, tittle, link)
-  date 	= data.xpath("//td").first.text
-	fecha	=  Date.strptime(date, "%Y-%m-%d")
-	
-	h = {"#{MONTHS[fecha.month]}_#{fecha.year}" => 
-				{	
-					link: 	link,
-					date: 	fecha,
-					month: 	MONTHS[fecha.month], 
-					year: 	fecha.year,
-					topics: "" #TODO
-				}
-	    }.to_json
-	puts h
-end
+# def build_json(data, tittle, link)
+#   date 	= data.xpath("//td").first.text
+# 	fecha	=  Date.strptime(date, "%Y-%m-%d")
 
-home = Nokogiri::HTML(open("https://github.com/madridrb/madridrb.github.io/wiki"))
-links = Hash.new
-home.css(".wiki-pages//a").each { |list| links["#{list.text.gsub(' ', '_')}.md"] = list['href'] }
+# 	h = {"#{MONTHS[fecha.month]}_#{fecha.year}" =>
+# 				{
+# 					link: 	link,
+# 					date: 	fecha,
+# 					month: 	MONTHS[fecha.month],
+# 					year: 	fecha.year,
+# 					topics: "" #TODO
+# 				}
+# 	    }.to_json
+# 	puts h
+# end
+
+page = Nokogiri::HTML(open("https://github.com/madridrb/madridrb.github.io/wiki"))
+links = page.css(".wiki-pages//a")
 
 #Save files
-links.each do |tittle, link|
-	unless tittle == "Home.md"
-		unless File.exists?("./#{tittle}")
-			reunion = Nokogiri::HTML(open("#{URL_BASE}#{link}"))
-			File.write("./#{tittle}", reunion.css(".markdown-body"))
+links.each do |link|
+	unless link.text == 'Home'
+		filename_html = "#{link.text.gsub(' ', '_')}.html"
+		edit_link = "https://github.com#{link['href']}"
+
+		File.open(filename_html, "w") do |file|
+			file.write(Nokogiri::HTML(open(edit_link)).css('.markdown-body'))
 		end
 
-	#Parsing files
-		file = File.open(tittle)
-		reunion_content = Nokogiri::XML(file)
-		build_json(reunion_content, tittle, link)
-		file.close
+		puts "#{filename_html} written to disk."
 	end
+end
+
+files = Dir["*.html"]
+
+
+files.each do |file|
+	file_content = File.open(file, "r").read
+	doc = Nokogiri::XML(file_content)
+
+	date, time, location = doc.css("table td")
+	date = Date.strptime(date, "%Y-%m-%d")
+	month = months[date.month]
+	year = date.year
+	tittle = (doc.css('h2')[0].text).strip
+	speaker_name = (doc.css('h2')[1].text).strip unless doc.css('h2')[1].nil?
+	speaker_twitter = doc.css('h2')[1].css('a')[1]['href'] unless doc.css('h2')[1].nil?
+	speaker = {:name => speaker_name, :twitter => speaker_twitter}
+
+	video_url = doc.css('h3').css('a')[1]['href'] if !doc.css('h3').css('a')[1].nil?
+
+	participants = Array.new
+	persons = doc.css('ul').css('.task-list').css('li').css('a')
+	persons.each do |speaker|
+		participants << {:name => speaker.text, :twitter => speaker['href']}
+	end
+
+	hash = {:month => month, :year => year, :date => date, :topics => {:tittle => tittle, :speakers => speaker},
+					:location => location.text, :video_url => video_url}
+
+	filename_json = "#{file.gsub('.html', '')}.json"
+	File.open(filename_json, "w") do |file|
+		file.write(hash.to_json)
+	end
+
+	puts "#{filename_json} written to disk."
+
 end
